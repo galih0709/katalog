6e4b32bb2f731af7bac544eb2700c629444eae31
galih0709 <38745592+galih0709@users.noreply.github.com>
2022-09-29 11:08:22 +0700

    Initial commit

---
+++ components/Logo.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="border border-solid mx-auto shadow-md rounded-lg w-64 p-1">
+    <img class="rounded-lg" src="~/static/pexels_logo_temp.jpg" alt="" />
+  </div>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,7 @@
+<template>
+  <div
+    class="font-sans min-w-full h-screen flex justify-center items-center content-center flex-wrap leading-7"
+  >
+    <Nuxt />
+  </div>
+</template>
